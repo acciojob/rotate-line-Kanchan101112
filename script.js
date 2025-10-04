65bdee572a585fc590b0e59d10228af4e9a20a00
Kanchan <kanchan96437@gmail.com>
2025-10-04 20:52:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+let angle = 0; // initial angle
+
+const line = document.getElementById('line');
+
+// Rotate the line every 20 milliseconds
+setInterval(() => {
+    angle += 2; // increase angle by 2 degrees
+    line.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`;
+}, 20);
